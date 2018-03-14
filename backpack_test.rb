b8494f4db841415193ba57eb6c657555abb9ed8d
likwokbiu <li_bill@ymail.com>
2018-03-14 13:22:38 -0400

    Rename my_func method to print_packing_list

--- backpack_test.rb
+++ backpack_test.rb
@@ -70,7 +70,7 @@ Day: monday, Weather: sunny
 - shirt
 - gym shoes
 - packed lunch"
-    assert_equal(expected, backpack.my_func)
+    assert_equal(expected, backpack.print_packing_list)
   end
 
 end
